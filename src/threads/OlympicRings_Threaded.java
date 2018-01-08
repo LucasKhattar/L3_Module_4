87a25543f0ba9c240f83c76a631d22c9afb80fbe
LucasKhattar <league@iMac.attlocal.net>
2018-01-07 17:28:13 -0800

    OlympicRings(IP)

--- src/threads/OlympicRings_Threaded.java
+++ src/threads/OlympicRings_Threaded.java
@@ -1,7 +1,15 @@
 package threads;
 
-public class OlympicRings_Threaded {
-	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
+import org.jointheleague.graphical.robot.Robot;
 
+public class OlympicRings_Threaded {
+	public static void main(String[] args) {
+		Robot one = new Robot(600, 700);
+		Robot two = new Robot(800, 700);
+		Robot three = new Robot(1000, 700);
+		Robot four = new Robot(900, 800);
+		Robot five = new Robot(700, 800);
+	}
+	// Make A Program that uses Threads and robots to draw the Olympic rings. One robot should draw one ring simultaneously with the other 4 robots.
 }
 
